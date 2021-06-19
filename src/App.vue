7675883f8baf6b0a2970e9f56308450ef1f3bbd4
Vignesh Paranthaman <vignesh-p@outlook.com>
2021-06-19 21:49:19 +0530

    cmt

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,26 @@
   button(@click="selectAndRead") Select file
   br
   | Orginal :
-  video(controls :src="inpt")
+  video(controls, :src="inpt")
   br
   button(@click="convert") Convert
   br
+  progress(max="1", :value="pgr")
+  | {{ (pgr * 100).toFixed(2) }}%
+  br
   | Output :
-  video(controls :src="opt")
+  video(controls, :src="opt")
   p
     | Using
+    |
     a(href="https://ffmpegwasm.github.io/") ffmpegwasm
     |
     | library to convert
 </template>
 
 <script lang="ts">
-import { defineComponent, Ref, ref } from "vue";
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { defineComponent, ref } from "vue";
+import { createFFmpeg } from "@ffmpeg/ffmpeg";
 import read from "./read";
 
 export default defineComponent({
@@ -33,28 +37,48 @@ export default defineComponent({
     const fil = ref(<File>(<unknown>null));
     let uint: Uint8Array;
     const inpt = ref("");
+    const pgr = ref(0);
     const opt = ref("");
     const inited = ref(false);
     const init = async () => {
       await ffmpeg.load();
       inited.value = true;
+      ffmpeg.setProgress(function ({ ratio }) {
+        pgr.value = ratio;
+      });
     };
     const selectAndRead = async () => {
       const [file, data] = await read();
       console.log(file);
       fil.value = file;
       uint = new Uint8Array(data);
-      inpt.value = URL.createObjectURL(new Blob([uint], {type:fil.value.type}));
+      inpt.value = URL.createObjectURL(
+        new Blob([uint], { type: fil.value.type })
+      );
     };
     const convert = async () => {
       if (!inited.value)
         return alert("Cannot convert untill the program is initilisied");
       ffmpeg.FS("writeFile", fil.value.name, uint);
-      await ffmpeg.run("-i", fil.value.name, "output.mp4");
-      opt.value = URL.createObjectURL(new Blob([ffmpeg.FS("readFile", "output.mp4")], {type:"video/mp4"}));
+      await ffmpeg.run(
+        "-i",
+        fil.value.name,
+        "-c:v",
+        "libx264",
+        "-preset",
+        "fast",
+        "-crf",
+        "22",
+        "-c:a",
+        "copy",
+        "output.mp4"
+      );
+      opt.value = URL.createObjectURL(
+        new Blob([ffmpeg.FS("readFile", "output.mp4")], { type: "video/mp4" })
+      );
     };
     init();
-    return { selectAndRead, convert, fil , opt, inpt};
+    return { selectAndRead, convert, fil, opt, inpt, pgr };
   },
 });
 </script>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+      name: 'Encoder',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+    },
+    publicPath:".",
+    outputDir:"dist/public"
+  }
