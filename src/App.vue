de1fdb15e8e50992642a627033e549c33f1d0eb8
Vignesh Paranthaman <vignesh-p@outlook.com>
2021-06-20 15:21:14 +0530

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,62 @@
 <template lang="pug">
 #app
-  h1 I Encoder
-  button(@click="selectAndRead") Select file
-  br
-  | Orginal :
-  video(controls, :src="inpt")
-  br
-  label Convert Oprions:
-  input(type="text", v-model="convertOptions", style="width: 250px")
-  br
-  button(@click="convert", :disabled="!converter.readyToConvert") Convert
-  br
-  progress(max="1", :value="converter.progress")
-  | {{ (typeof converter.progress == "number")?(converter.progress * 100).toFixed(2)+"%":"Loading..." }}
-  br
-  | Output :
-  video(controls, :src="opt")
-  br
-  a( :href="opt" download :disabled="!opt")
-    button(:disabled="!opt") Save File
-  p
-    | Using
-    |
-    a(href="https://ffmpegwasm.github.io/") ffmpegwasm
-    |
-    | library to convert
+  .headder
+    h1 I Encoder
+  .content
+    .orginal
+      h3 Orginal
+      button(@click="selectAndRead") Select file
+      br
+      video(controls, :src="inpt")
+    .options
+      label Convert Oprions:
+      input(type="text", v-model="convertOptions", style="width: 250px")
+      br
+      button(@click="convert", :disabled="!converter.readyToConvert") Convert
+      br
+      progress(max="1", :value="converter.progress")
+      | {{ typeof converter.progress == 'number' ? (converter.progress * 100).toFixed(2) + '%' : 'Loading...' }}
+      br
+    .output
+      h3 Output
+      video(controls, :src="opt")
+      br
+      a(:href="opt", download, :disabled="!opt")
+        button(:disabled="!opt") Save File
+  .footer
+    p
+      | Using
+      |
+      a(href="https://ffmpegwasm.github.io/") ffmpegwasm
+      |
+      | library to convert
 </template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  .content {
+    display: flex;
+    flex-direction: column;
+    div {
+      margin: 1vmin auto 1vmin auto;
+      background-color: rgb(236, 236, 236);
+      width:50vmin;
+      padding:20px;
+      border-radius:2vmin;
+      box-shadow: grey 0px 0px 0.7vmin;
+      video {
+        width: 50vmin;
+      }
+    }
+  }
+}
+</style>
 
 <script lang="ts">
 import { computed, defineComponent, ref } from "vue";
@@ -78,14 +109,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
